be1fdc5743fe68ed23f63c9009d68a611e8a0f0a
gmart020 <gmart020@ucr.edu>
2016-05-10 11:42:55 -0700

    Updated main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
 
+int main(){
+  char HOSTNAME[128];
+  gethostname(HOSTNAME, sizeof HOSTNAME);
+  bool REPEAT = true;
+  
+  while (REPEAT){
+    string input;
+    cout << getlogin() << "@" << HOSTNAME << "$ ";
+    getline(cin, input);
+    
+    cout << input << endl;
+  }
+}
